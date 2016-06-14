7a0b3df988a5b4eb324bfe199d3888e707e48b18
t-h-e-c-h-e-f <thechef@reasonableconspiracy.com>
2016-06-14 14:13:22 +0000

    Add files via upload

---
+++ install_rdp.cmd
@@ -0,0 +1,10 @@
+@echo off
+echo Installing nxlog
+msiexec /passive /i "\\yourserver\yourshare\nxlog\nxlog.msi"
+echo copying configuration
+move "C:\Program Files (x86)\nxlog\conf\nxlog.conf" "C:\Program Files (x86)\nxlog\conf\nxlog.conf.default"
+copy "\\yourserver\yourshare\nxlog\nxlog.rdp" "C:\Program Files (x86)\nxlog\conf\nxlog.conf"
+echo Starting nxlog Service
+net start nxlog
+echo Complete!
+exit
